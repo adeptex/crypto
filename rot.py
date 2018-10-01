011154f532762c6261432a4f0f3f8525cbf18c14
adeptex <adeptex@users.noreply.github.com>
2018-10-01 11:36:44 +0200

    Rewrite and improve
    Python3
    Upper/Lower case
    Usage instructions

--- rot.py
+++ rot.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys, string
 
-cipher = sys.argv[1].upper()
+if len(sys.argv) < 2:
+	sys.exit('Usage: rot.py message')
+
+cipher = sys.argv[1]
 
 for rot in range(1, 27):
-	plain = ""
-	for c in cipher:
-		try:
-			i = string.uppercase.index(c)
-			i = (i + rot) % len(string.uppercase)
-			plain += string.uppercase[i]
-		except:
-			plain += c
+	charset_lowercase = string.ascii_lowercase[rot:] + string.ascii_lowercase[:rot]
+	charset_uppercase = string.ascii_uppercase[rot:] + string.ascii_uppercase[:rot]
+	roted = ''
 
-	print "[%02d] %s" % (rot, plain)
+	for ch in cipher:
+		if ch in string.ascii_lowercase:
+			roted += charset_lowercase[string.ascii_lowercase.index(ch)]
+		elif ch in string.ascii_uppercase:
+			roted += charset_uppercase[string.ascii_uppercase.index(ch)]
+		else:
+			roted += ch
+
+	print('[%02d] %s' % (rot, roted))
